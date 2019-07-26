5541f5d9b99c9405576e335e575fe8b3ab943a98
PeterGhimself <petergranitski@gmail.com>
2019-07-25 20:45:00 -0400

    [#4] Add ability to query DB from textarea box and see results by redirecting to new page query.php

--- finalProject/demo.php
+++ finalProject/demo.php
@@ -8,6 +8,7 @@
 <em><?echo date("D M d, Y H:i:s", time())?></em>
 <p>Current PHP version:
 <em><?echo  phpversion()?></em>
+<br>
 <?php
 $servername = "irc353.encs.concordia.ca";
 $username = "irc353_1";
@@ -21,6 +22,14 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
-?></body>
+?>
+<form action="query.php" method="GET">
+    <textarea name="userQuery" cols="100" rows="5">Enter your SQL query here...</textarea><br>
+    <input type="submit" value="Run Query on LIVE DB"/>
+</form>
+<!-- For now the results appear on another page which we redirect to -->
+<!-- But ideally we'd like to stay on the same page and append the results in the p tag below -->
+<p id="queryResult"></p>
+</body>
 </html>
 
---
+++ finalProject/query.php
@@ -0,0 +1,51 @@
+<?php
+$sql = $_GET['userQuery'];
+
+$servername = 'irc353.encs.concordia.ca';
+$username = 'irc353_1';
+$password = 'databas3';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
+
+echo "Connected successfully<br>";
+
+echo "Executing: $sql<br>";
+echo '----------' . str_repeat('--', strlen($sql)) . '<br>';
+
+// first use the correct DB
+$useDB = mysqli_query($conn, "use irc353_1");
+
+// then execute the users query
+$result = mysqli_query($conn, $sql);
+
+if (!$result) {
+    $message = 'Invalid query: ' . mysqli_error($conn) . '<br>';
+    die($message);
+} else {
+    $row = mysqli_fetch_array($result);
+
+    echo "Result:<br>";
+
+    // Process all rows
+    $i = 0;
+    while($row = mysqli_fetch_array($result)) {
+        $i++;
+        //echo "row: $row<br>";
+        echo "| ";
+        foreach ($row as $key => $value) {
+            if (!is_numeric($key)) {
+                echo "$key: $value | ";
+            }
+        }
+        echo '<br>';
+    }
+    echo '--------------------<br>';
+    echo "Total rows: $i";
+}
+?>
